81f1cd0b75bbe122a7f3da5715012e06735b531a
Lean Sibal <lsibal@apdgroup.com>
2018-10-29 02:17:17 +0800

    Plugin framework

--- plugin.php
+++ plugin.php
@@ -1,8 +1,64 @@
 <?php
-/*
-Plugin Name: Youtube Playlist
-PluginURI: http://www.renesejling.dk
-Description: Show youtube as playlist
-Author: René Sejling
-Version: 1.0.0
-*/
+
+if( !defined( 'ABSPATH' ) ) exit;
+
+if( class_exists( 'WordPressPlugin' ) ) return;
+
+class WordPressPlugin 
+{
+
+	public $version = "1.0.0";
+
+  public $actions = [];
+
+  public $filters = [];
+
+  public $shortcodes = [];
+
+	public function __construct() {
+		self::setup_actions();
+		self::setup_filters();
+	}
+
+	protected function setup_actions() {
+    if( !empty( $this->shortcodes ) ) $this->actions['setup_shortcodes'] = 'init';
+    if( !empty( $this->styles ) ) $this->actions['setup_styles'] = 'wp_enqueue_scripts';
+		foreach( $this->actions as $function => $action ) {
+      if( !method_exists( $this, $function ) ) continue;
+			$tag = !empty( $action['tag'] ) ? $action['tag'] : $action;
+			$priority = !empty( $action['priority'] ) ? $action['priority'] : 10;
+			$accepted_args = !empty( $action['accepted_args'] ) ? $action['accepted_args'] : 1;
+      add_filter( $tag, [ $this, $function ], $priority, $action );
+		}
+	}
+
+	protected function setup_filters() {
+		foreach( $this->filters as $function => $filter ) {
+      if( !method_exists( $this, $function ) ) continue;
+			$hook = !empty( $filter['tag'] ) ? $filter['tag'] : $filter;
+			$priority = !empty( $filter['priority'] ) ? $filter['priority'] : 10;
+			$accepted_args = !empty( $filter['accepted_args'] ) ? $filter['accepted_args'] : 1;
+      add_filter( $hook, [ $this, $function ], $priority, $accepted_args );
+		}
+	}
+
+  public function setup_shortcodes() {
+    foreach( $this->shortcodes as $tag => $callback ) {
+      if( !method_exists( $this, $callback ) ) continue;
+      add_shortcode( $tag, [ $this, $callback ] );
+    }
+  }
+
+  public function setup_styles() {
+    foreach( $this->styles as $handle => $src ) {
+      $path = !empty( $src['src'] ) ? $src['src'] : $src;
+      if( !file_exists( plugin_dir_path( __FILE__ ) . $src ) && !filter_var( $src, FILTER_VALIDATE_URL ) ) continue;
+      $url = filter_var( $src, FILTER_VALIDATE_URL ) ? $path : plugins_url( $path, __FILE__ );
+      $deps = !empty( $src['deps'] ) ? $src['deps'] : [];
+      $ver = !empty( $src['ver'] ) ? $src['ver'] : $this->version;
+      $media = !empty( $src['media'] ) ? $src['media'] : 'all';
+      wp_register_style( $handle, $url, $deps, $ver, $media );
+    }
+  }
+
+}
---
+++ youtube-playlist.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Plugin Name: Youtube Playlist
+PluginURI: http://www.renesejling.dk
+Description: Show youtube as playlist
+Author: René Sejling
+Version: 1.0.0
+*/
+require "plugin.php";
+
+class YoutubePlaylist extends WordPressPlugin
+{
+
+  public $shortcodes = [
+    'youtube-playlist' => 'youtube_playlist_shortcode'
+  ];
+
+  public $styles = [
+    'youtube-playlist' => 'assets/css/youtube-playlist.css'
+  ];
+
+  public function youtube_playlist_shortcode() {
+    wp_enqueue_style( 'youtube-playlist' );
+    wp_enqueue_style( 'bootstrap' );
+    ob_start();
+    echo "hello";
+    return ob_get_clean();
+  }
+
+}
+
+new YoutubePlaylist;
